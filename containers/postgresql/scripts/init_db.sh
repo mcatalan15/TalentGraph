7ccbd622a889626c29ebd46dd2223f2b776b5f78
Marc Catalán <74063453+mcatalan15@users.noreply.github.com>
2026-05-28 14:21:46 +0200

    Metamask and Initial SignIn/LogIn (#6)
    * Adding Adminer at http://localhost:8081 + users and SIWE tables for signin
    
    * changing style.css to black and white minimal view + Adding wallet signin and started 42Oauth using SPA (or at least trying)
    
    * removing comments
    
    * updating signin
    
    * added and commented adminer container
    
    * added and commented adminer container at docker-compose.yaml
    
    * Removing Viem installation from Dockerfile and added to package.json

--- containers/postgresql/scripts/init_db.sh
+++ containers/postgresql/scripts/init_db.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-# Wait for PostgreSQL to be ready
-until pg_isready -h postgresql -p 5432; do
-  echo "Waiting for PostgreSQL to be ready..."
-  sleep 2
-done
+# # Wait for PostgreSQL to be ready
+# until pg_isready -h postgresql -p 5432; do
+#   echo "Waiting for PostgreSQL to be ready..."
+#   sleep 2
+# done
+
+echo "PostgreSQL is ready! Creating tables..."
+
+# Executing SQL tables and fields creation
+psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
+    -- Principal Users table
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        wallet_address VARCHAR(42) UNIQUE NOT NULL,
+        name VARCHAR(100),
+        email VARCHAR(255) UNIQUE,
+        intra_login VARCHAR(255) UNIQUE,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+
+    -- Table for SIWE security (Control Nonces)
+    CREATE TABLE IF NOT EXISTS siwe_nonces (
+        nonce VARCHAR(12) PRIMARY KEY,
+        wallet_address VARCHAR(42) NOT NULL,
+        expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+
+    -- Index for cleaning expired nonces quickly and efitiently
+    CREATE INDEX IF NOT EXISTS idx_siwe_nonces_expory ON siwe_nonces(expires_at);
+EOSQL
+
+echo "Database schema initialized successfully!"
